bb34b49ef5baebb47da9fe3f45eb107953be341b
Kristian Portillo <portillk@gmail.com>
2020-07-10 08:56:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,10 +6,9 @@ def key_for_min_value(name_hash)
  key = nil 
   lowest_value = 0 
   name_hash.each do |key, value|
-     if lowest_value == nil 
+     if lowest_value == 0 || value < lowest_value 
        value =  lowest_value
-      elsif value < lowest_value
-      value = lowest_value
+     lowest_value = key
 end 
 end 
 end
